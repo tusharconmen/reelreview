7459d7f7597f7ce0bdeffc28836a5cd93a67112d
Tushar <tushar@Tushars-MacBook-Air.local>
2025-09-20 23:58:39 +0530

    basic app working

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/context/AuthContext';
 import RootNavigator from './src/navigation/RootNavigator';
-import './global.css';
 
 export default function App() {
   return (
